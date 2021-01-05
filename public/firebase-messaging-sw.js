3ade98267ecfcb10cf1efe5f6cc6c0287262ad09
Grinseteddy <annegret.kampe@arcor.de>
2021-01-05 10:19:09 +0100

    Geht noch nicht

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,12 +1,18 @@
-import('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-import('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+//import firebase from "firebase/app";
+//import firebase from "firebase/messaging"
+
+importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js");
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+);
 
 // eslint-disable-next-line no-undef
 firebase.initializeApp({
-    messagingSenderId: "236592691654",
-    projectId: "projectforwebnotifications",
-    apiKey: "AAAANxYF-cY:APA91bHDi9By8eeWWNJOCz4Eug-OCThXgcZ5014hxS5OhAKgWgp1Q2NsZF5LIQL-jHdZny7MwSN7B2T1qzp-3QsqIqHyg6LhB4qNeyhGwWyzdPI7lMDBe9Klfrgl2pkVc0Cym2oJF0hq",
-    appId: "projectforwebnotifications"
+    "messagingSenderId": "236592691654",
+    "projectId": "projectforwebnotifications",
+    "apiKey": "AAAANxYF-cY:APA91bFupw59Qn6e9G8ocGJloc_1Sc3_9GZnqsXNlyi-AyZvC7678Ppw8syBIWz255-IQYvR9rWLcerakND8F2dx-C6KYHcosP_m91X_DD0qcEd-e_19fImv-xx6XgmUoHEbTkp-bnar",
+    "appId": "projectforwebnotifications"
 });
 
 // eslint-disable-next-line no-undef
